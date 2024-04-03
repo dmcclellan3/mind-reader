4fe1aeaea423d91c29b8505b89c5f26b33ec4508
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-03 16:00:08 -0400

    saving

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // main.js
+let text = document.getElementById("top-text")
+text.textContent = "Pick a Number from 0-99"
+let screen = "page 1"
+
+//add switch statement for each page
 
 function init(){
-    console.log("hello world!");
+    
   }
   
   init()
