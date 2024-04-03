098b696cdf1db070cb0a129974b16d326260465f
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-03 16:56:53 -0400

    saving

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 // main.js
 let text = document.getElementById("top-text")
-text.textContent = "Pick a Number from 0-99"
-let screen = "page 1"
+let screen = 1
+
+switch (screen){
+    case 1:
+        text.textContent = "I can read your mind"
+        break;
+    
+    case 2: 
+        text.textContent = "Pick a number between 0-99"
+        break;
+    
+    case 3:
+        text.textContent = "Add both digits together to get a new number"
+        break;
+
+    case 4:
+        text.textContent = "Subtract your new number from the original number"
+        break;
+
+    case 5:
+        text.textContent = ""
+
+
+
+
+
+}
 
 //add switch statement for each page
 
