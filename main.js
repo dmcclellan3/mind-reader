f642fe04365ee0c303f468408d6bb8046be8a862
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-04 16:15:27 -0400

    saving

--- main.js
+++ main.js
@@ -1,34 +1,53 @@
 // main.js
-let text = document.getElementById("top-text")
+let topText = document.getElementById("top-text")
+let subText = document.getElementById("sub-text")
+let lowText = document.getElementById("low-text")
 let stagesBtn = document.getElementById("stages-button")
+let nextBtn = document.getElementById("next-button")
 stagesBtn.addEventListener("click", () => { 
   screen = screen + 1
-  screenChng()
+  screenChange()
 }) 
+nextBtn.addEventListener("click", () => {
+screen = screen + 1
+screenChange()
+})
+
 let screen = 1
 
-function screenChng() {
+function screenChange() {
 
     switch (screen){
         case 1:
-            text.textContent = "I can read your mind"
+            topText.textContent = "I can read your mind"
+            nextBtn.classList.add("d-none")
+
             break;
         
         case 2: 
-            text.textContent = "Pick a number between 0-99"
+            topText.textContent = "Pick a number between 0-99"
+            nextBtn.classList.remove("d-none")
+            stagesBtn.classList.add("d-none")
+
 
             break;
         
         case 3:
-            text.textContent = "Add both digits together to get a new number"
+            topText.textContent = "Add both digits together to get a new number" 
+            subText.textContent = "Ex. 14 is 1 + 4 = 5"
+            lowText.textContent = "Click next to proceed"
+            stagesBtn.classList.add("d-none")
             break;
 
         case 4:
-            text.textContent = "Subtract your new number from the original number"
+            topText.textContent = "Subtract your new number from the original number"
+            subText.textContent = "Ex. 14 - 5 = 9"
+            lowText.textContent = "Click next to proceed"
             break;
 
         case 5:
-            text.textContent = ""
+            subText.textContent = "Find your new number"
+            lowText.textContent = "Note the symbol beside the number"
 
 
 
@@ -38,7 +57,7 @@ function screenChng() {
 
 }
     
-screenChng()
+screenChange()
 
 
 function init(){
