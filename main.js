f80e9f677156c5176d2a517a02a1ad00b0248c65
dmcclellan3 <wdmcclellan3@gmail.com>
2024-04-04 14:41:07 -0400

    saving

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
 // main.js
 let text = document.getElementById("top-text")
+let stagesBtn = document.getElementById("stages-button")
+stagesBtn.addEventListener("click", () => { 
+  screen = screen + 1
+  screenChng()
+}) 
 let screen = 1
 
-switch (screen){
-    case 1:
-        text.textContent = "I can read your mind"
-        break;
-    
-    case 2: 
-        text.textContent = "Pick a number between 0-99"
-        break;
-    
-    case 3:
-        text.textContent = "Add both digits together to get a new number"
-        break;
+function screenChng() {
 
-    case 4:
-        text.textContent = "Subtract your new number from the original number"
-        break;
+    switch (screen){
+        case 1:
+            text.textContent = "I can read your mind"
+            break;
+        
+        case 2: 
+            text.textContent = "Pick a number between 0-99"
 
-    case 5:
-        text.textContent = ""
+            break;
+        
+        case 3:
+            text.textContent = "Add both digits together to get a new number"
+            break;
+
+        case 4:
+            text.textContent = "Subtract your new number from the original number"
+            break;
+
+        case 5:
+            text.textContent = ""
 
 
 
 
+
+    }
 
 }
-
+    
+screenChng()
 
 
 function init(){
